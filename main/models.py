d0e71d9b3c6b6e5d31932d98844138dc140258af
Andrew11111999 <below3631@gmail.com>
2025-11-07 18:00:12 +0200

    adedd 'main' application with models ['Category', 'Size', 'Product']

---
+++ main/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils.text import slugify
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100, unique=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Size(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+# class ProductSize(models.Model)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100, unique=True)
