46376941aac082c8210ca452cf27683d831bb1eb
Andrew11111999 <below3631@gmail.com>
2025-11-12 18:38:00 +0200

    update the code

--- main/views.py
+++ main/views.py
@@ -1,7 +1,13 @@
+from itertools import product
+from lib2to3.fixes.fix_input import context
+from re import template
+
 from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView, DetailView
 from django.http import HttpResponse
 from django.template.response import TemplateResponse
+from unicodedata import category
+
 from .models import Category, Product, Size
 from django.db.models import Q
 
@@ -22,3 +28,64 @@ class IndexView(TemplateView):
         return TemplateResponse(request, self.template_name, context)
 
 
+class CatalogView(TemplateView):
+    template = 'main/base.html'
+
+    FILTER_MAPPING = {
+        'color': lambda queryset, value: queryset.filter(color__iexact=value),
+        'min_price': lambda queryset, value: queryset.filter(price_gte=value),
+        'max_price': lambda queryset, value: queryset.filter(price_lte=value),
+        'size': lambda queryset, value: queryset.filter(product_size__size__name=value),
+    }
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_slug = kwargs.get('category_slug')
+        categories = Category.objects.all()
+        products = Product.objects.all().order_by('-created_at')
+        current_category = None
+
+        if category_slug:
+            current_category = get_object_or_404(Category, slug=category_slug)
+            products = products.filter(category=current_category)
+
+        query = self.request.GET.get('q')
+        if query:
+            products = products.filter(
+                Q(name_icontains=query) | Q(description_icontains=query)
+            )
+        filter_params = {}
+        for param, filter_func in self.FILTER_MAPPING.items():
+            value = self.request.GET.get(param)
+            if value:
+                products = filter_func(products, value)
+                filter_params[param] = value
+            else:
+                filter_params[param] = ''
+
+        filter_params['q'] = query or ''
+
+        context.update({
+            'categories': categories,
+            'products': products,
+            'current_category': category_slug,
+            'filter_params': filter_params,
+            'sizes': Size.objects.all(),
+            'search_query': query or ''
+        })
+
+        if self.request.GET.get('show_search') == 'true':
+            context['show_search'] = True
+        elif self.request.GET.get('reset_search') == 'true':
+            context['reset_search'] = True
+
+        return context
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        if request.headers.get('HX-Request'):
+            if context.get('show_search'):
+                return TemplateResponse(request, 'main/search_input.html', context)
+            elif context.get('reset_search'):
+                return TemplateResponse(request, 'main/search_button.html', {})
+            template = 'main/filter_modal.html'
