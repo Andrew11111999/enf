b74d7c1b5991c63d38c10ee2b703fd63acc2cea5
Andrew11111999 <below3631@gmail.com>
2025-11-22 11:30:48 +0200

    update code

--- cart/models.py
+++ cart/models.py
@@ -1,2 +1,13 @@
 from django.db import models
+from django.contrib.sessions.models import Session
+from main.models import Product, ProductSize
+from decimal import Decimal
 
+
+class Cart(models.Model):
+    session_key = models.CharField(max_length=40, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Cart {self.session_key}'
